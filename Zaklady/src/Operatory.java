3675b9b73d937b648a0d4542970168530e461b1d
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-11-23 10:01:48 +0100

    operatory

---
+++ Zaklady/src/Operatory.java
@@ -0,0 +1,29 @@
+
+public class Operatory {
+
+	public static void main(String[] args) {
+		
+		//aritmeticke
+		System.out.println(10 + 5);
+		System.out.println(10 - 5);
+		System.out.println(10 * 5);
+		System.out.println(10 / 3.0);
+		System.out.println(10 % 3);
+		
+		//relacne
+		System.out.println(10 > 5);
+		System.out.println(10 < 5);
+		System.out.println(10 >= 5);
+		System.out.println(10 <= 5);
+		System.out.println(10 == 5);
+		System.out.println(10 != 5);
+		
+		//logicke
+		System.out.println((10 > 5) && (10 != 5));//logicky sucin
+		System.out.println((10 > 5) || (10 == 5));//logicny sucet
+		System.out.println(!(10 > 5) && (10 != 5));
+		
+		
+		
+	}
+}
