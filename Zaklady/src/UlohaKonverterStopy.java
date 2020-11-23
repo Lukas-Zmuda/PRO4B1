5cdeff80b738804d38007bba43e768b8d36b0f41
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-11-23 10:07:31 +0100

    uloha konverter stopy na metre

---
+++ Zaklady/src/UlohaKonverterStopy.java
@@ -0,0 +1,12 @@
+
+public class UlohaKonverterStopy {
+
+	public static void main(String[] args) {
+		int stopy = 10000;
+		
+		double metre = stopy * 0.305;
+		
+		System.out.println(stopy + " stop je " + metre + " metrov.");
+	}
+
+}
