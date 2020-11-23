89d734da799929147461086b1df4b5d05ff589ea
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-11-23 10:21:49 +0100

    konverter so vstupom

--- Zaklady/src/UlohaKonverterStopy.java
+++ Zaklady/src/UlohaKonverterStopy.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class UlohaKonverterStopy {
 
 	public static void main(String[] args) {
-		int stopy = 10000;
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("--- Konverter stopy -> metre ---");
+		System.out.println("Zadaj stopy: ");
+		int stopy = sc.nextInt();
+				
 		double metre = stopy * 0.305;
 		
 		System.out.println(stopy + " stop je " + metre + " metrov.");
