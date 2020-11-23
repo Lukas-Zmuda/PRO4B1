8e725923ccf5ceee88b1e6cd7d8983d41a58a1f6
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2020-11-23 09:51:33 +0100

    datove typy

---
+++ Zaklady/src/DatoveTypy.java
@@ -0,0 +1,53 @@
+
+public class DatoveTypy {
+
+	public static void main(String[] args) {
+
+		//celociselne DT
+		//byte, short, int, long
+		int cislo;
+		short den1;
+		byte mocnaPremenna;
+		
+		cislo = 10;
+		
+		int x = 10, y = 12;
+		short a, b, c;
+		
+		System.out.println(cislo);
+		System.out.println("cislo = " + cislo);
+		
+		//desatinne cisla
+		float f = 12.36f;
+		double d = 12.3789;
+		System.out.println("d = " + d);
+		
+		//znak
+		char zav = '@';
+		
+		zav = 65;
+		System.out.println("zavinac: " + zav);
+		
+		//logicke hodnoty
+		boolean prsi = false;
+		
+		byte bb = 10;
+		short sh = bb;
+		
+		System.out.println("sh = " + sh);
+		
+		bb = (byte)sh;
+		
+		//retazce
+		String meno = "Lukas";
+		
+		System.out.println(meno);
+		
+		
+		
+		
+		
+
+	}
+
+}
