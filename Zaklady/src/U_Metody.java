a924b04f4236f68d71870d519de2e9027ca3dffb
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-03-08 08:50:27 +0100

    uloha metody

---
+++ Zaklady/src/U_Metody.java
@@ -0,0 +1,20 @@
+
+public class U_Metody {
+
+	public static void main(String[] args) {
+		
+		int x = mocnina(10, 3); // x = 1000
+		String[] mena = {"Lukas", "Fero", "Ivana", "Petra", "Michal"};
+		poleOdzadu(mena); //Michal, Petra, Ivana, Fero, Lukas
+		int[] cisla = {1, 2, 3, 4, 5, 6, 7};
+		int[] cislaNaopak = opacnePole(cisla);
+		
+
+	}
+
+	static int mocnina(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
